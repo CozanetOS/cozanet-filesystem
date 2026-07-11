4d00ea68cffa0c5d712c9b6f5ac9e7aba1e243f8
CozyCrypto <fassdavid722@gmail.com>
2026-07-11 01:20:19 +0100

    feat: scaffold

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './types.js';
+export { FileReader } from './Read/reader.js';
+export { FileWriter } from './Write/writer.js';
+export { FileSearch } from './Search/search.js';
+export { FileOrganizer } from './Organize/organizer.js';
+export { BackupEngine } from './Backup/backup.js';
+export { FileSyncEngine } from './Sync/sync.js';
